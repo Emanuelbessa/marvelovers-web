edefaeb60c3708cd06e7868a431d445253b43fac
Emanuel Bessa <41740085+Emanuelbessa@users.noreply.github.com>
2021-08-03 14:41:02 -0300

    feat(web): :sparkles: add homepage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContentComponent } from '@shared/components/layout/content/content.component';
+import { HomeModule } from './pages/home/home.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    component: ContentComponent,
+    children: [
+      {
+        path: '',
+        loadChildren: (): Promise<HomeModule> => import('./pages/home/home.module').then((m) => m.HomeModule),
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/pages/home/home.routing.ts
@@ -0,0 +1,17 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: '',
+        component: HomeComponent,
+        data: {
+          title: 'Home',
+        },
+      },
+    ],
+  },
+];
